7ea363284ed03f2bf85a1d8ae1c38b7016b1febd
anshshr <anshshrivastav24@gmail.com>
2025-02-28 22:14:30 +0530

    fetch client route

--- routes/project.js
+++ routes/project.js
@@ -1,29 +1,29 @@
-const express=require('express');
+const express = require('express');
 const Project = require('../models/project.model');
 const Client = require('../models/client.model');
 const freelance = require("../models/freelance.model")
 
-const router=express.Router();
-router.get("/",(req,res)=>{
+const router = express.Router();
+router.get("/", (req, res) => {
     res.json({
-        message:"This is project Page"
+        message: "This is project Page"
     })
 })
-router.post("/createProject",async (req,res)=>{
+router.post("/createProject", async (req, res) => {
     try {
-        const {title,description,client,budget,deadline,skillsRequired,projectImage}=await req.body;
-        if(!title || !description || !client || !budget || !deadline || !skillsRequired ){
+        const { title, description, client, budget, deadline, skillsRequired, projectImage } = await req.body;
+        if (!title || !description || !client || !budget || !deadline || !skillsRequired) {
             return res.json({
-                message:"Please fill all the fields"
+                message: "Please fill all the fields"
             })
         }
-        const newClient=await Client.findById(client);
-        if(!newClient){
+        const newClient = await Client.findById(client);
+        if (!newClient) {
             return res.json({
-                message:"Client not found"
+                message: "Client not found"
             })
         }
-        const newProject=new Project({
+        const newProject = new Project({
             title,
             description,
             client,
@@ -35,97 +35,97 @@ router.post("/createProject",async (req,res)=>{
         await newProject.save();
         newClient.projectsPosted.push(newProject._id);
         await newClient.save();
-        const freelancers=await freelance.find({skills:{$in:skillsRequired}});
-        if(freelancers){
-            freelancers.forEach(async (freelancer)=>{
+        const freelancers = await freelance.find({ skills: { $in: skillsRequired } });
+        if (freelancers) {
+            freelancers.forEach(async (freelancer) => {
                 freelancer.newProjectNotification.push({
-                    ProjectId:newProject._id,
-                    isRead:false
+                    ProjectId: newProject._id,
+                    isRead: false
                 })
                 await freelancer.save();
             })
         }
         res.status(200).json({
-            sucess:true,
-            message:"New Project Created sucessfully",
+            sucess: true,
+            message: "New Project Created sucessfully",
             newProject,
             newClient,
             freelancers
         })
     } catch (error) {
         res.json({
-            message:"Error in creating project "+error.message
+            message: "Error in creating project " + error.message
         })
     }
 })
-router.get("/getProjects",async (req,res)=>{
+router.get("/getProjects", async (req, res) => {
     try {
-        const projects=await Project.find();
+        const projects = await Project.find();
         res.status(200).json({
-            sucess:true,
+            sucess: true,
             projects
         })
     } catch (error) {
         res.json({
-            message:"Error in getting projects "+error.message
+            message: "Error in getting projects " + error.message
         })
     }
 })
-router.get("/getProject/:id",async (req,res)=>{
+router.get("/getProject/:id", async (req, res) => {
     try {
-        const project=await Project.findById(req.params.id);
+        const project = await Project.findById(req.params.id);
         res.status(200).json({
-            sucess:true,
+            sucess: true,
             project
-        })  
+        })
     } catch (error) {
         res.json({
-            message:"Error in getting project "+error.message
+            message: "Error in getting project " + error.message
         })
     }
 })
-router.get("/getAllClientProject/:id",async (req,res)=>{
+router.get("/getAllClientProject/:id", async (req, res) => {
     try {
-        const client=await Client.findById(req.params.id);
-        if(!client){
+        const client = await Client.findById(req.params.id);
+        if (!client) {
             return res.json({
-                message:"Client not found"
+                message: "Client not found"
             })
         }
-        const project=await Project.find({
-            client:req.params.id
+        const project = await Project.find({
+            client: req.params.id
         })
         res.status(200).json({
-            sucess:true,
+            sucess: true,
             project
         })
     } catch (error) {
         res.json({
-            message:"Error in getting project "+error.message
+            message: "Error in getting project " + error.message
         })
     }
 })
-router.post("/updateProject/:id",async (req,res)=>{
+router.post("/updateProject/:id", async (req, res) => {
     try {
-        const project=await Project.findById(req.params.id);
-        
-        if(!project){
+        const project = await Project.findById(req.params.id);
+
+        if (!project) {
             return res.status(400).json({
-                message:"Project not found"
+                message: "Project not found"
             })
         }
-        const updateProject=await Project.findByIdAndUpdate(req.params.id,req.body,{
-            new:true
+        const updateProject = await Project.findByIdAndUpdate(req.params.id, req.body, {
+            new: true
         })
         res.status(200).json({
-            sucess:true,
-            message:"Project updated sucessfully",
+            sucess: true,
+            message: "Project updated sucessfully",
             updateProject
         })
     } catch (error) {
         res.status(400).json({
-            message:"Error in updating project "+error.message
+            message: "Error in updating project " + error.message
         })
     }
 })
-module.exports=router;
+module.exports = router;
